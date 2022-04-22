b6a801eb08c55498228226340949cdac5bdc258e
Iuri Dias <iuridias.mw@gmail.com>
2022-04-21 22:43:45 -0300

    projeto final adicionado

---
+++ README.md
@@ -0,0 +1,16 @@
+# API com JWT
+Nesse projeto, novamente criei uma API Rest e inclui autenticação de tokens utilizando JWT.
+
+Nesse módulo pude aprender a diferença de sistemas de logins com sessões para sistema de logins com tokens. E também pude apronfudar entre:
+-- Criação de tokens;
+-- Criar senhas aleatórias para chaves;
+-- Utilizando variváveis de ambiente (dotEnv);
+-- Estratégias de autenticação com tokens;
+-- Adicionar tempo de expiração nos tokens;
+-- Tratamento de erros de autenticação;
+-- Login e Logout com tokens;
+-- Criação de blacklist para invalidar tokens utilizando Redis.
+
+Todo projeto foi criado com base no seguinte curso da Alura:
+
+- Node.js e JWT: autenticação com tokens
---
+++ src/usuarios/estrategias-autenticacao.js
@@ -0,0 +1,66 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const BearerStrategy = require('passport-http-bearer').Strategy;
+
+const Usuario = require('../usuarios/usuarios-modelo');
+const { InvalidArgumentError } = require('../erros')
+
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const blacklist = require('../../redis/manipula-blacklist');
+
+function verificaUsuario (usuario) {
+  if (!usuario) {
+    throw new InvalidArgumentError('Email não encontrado.');
+  }
+}
+
+async function verificaSenha (senha, senhaHash) {
+  const senhaValida = await bcrypt.compare(senha, senhaHash);
+  if (!senhaValida) {
+    throw new InvalidArgumentError('Email ou senha inválidos.')
+  }
+}
+
+async function verificaTokenBlacklist (token) {
+  const tokenNaBlacklist = await blacklist.contemToken(token)
+  if (tokenNaBlacklist) {
+    throw new jwt.JsonWebTokenError('Token Inválido por logout!');
+  }
+}
+
+passport.use(
+  new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'senha',
+    session: false
+  }, async (email, senha, done) => {
+    try {
+      const usuario = await Usuario.buscaPorEmail(email);
+      verificaUsuario(usuario);
+      await verificaSenha(senha, usuario.senhaHash);
+
+      done(null, usuario);
+    }
+    catch (error) {
+      done(error);
+    }
+  })
+);
+
+passport.use (
+  new BearerStrategy(
+    async (token, done) => {
+      try {
+        await verificaTokenBlacklist(token);
+        const payload = jwt.verify(token, process.env.CHAVE_JWT);
+        const usuario = await Usuario.buscaPorId(payload.id);
+        done(null, usuario, { token: token });
+      }
+      catch (error) {
+        done(error);
+      }
+    }
+  )
+)
